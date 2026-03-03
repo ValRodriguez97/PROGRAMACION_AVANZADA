3594f8215bc287bf7348a2bd53df14bb15d88894
ValRodriguez97 <valentina.rodriguezc1@uqvirtual.edu.co>
2026-03-02 20:32:29 -0500

    Creación del enum Genero

---
+++ Taller-API/src/main/java/co/edu/uniquindio/Taller_API/entity/Desarrolladora.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.Taller_API.entity;
+
+public class Desarrolladora {
+}
---
+++ Taller-API/src/main/java/co/edu/uniquindio/Taller_API/entity/Genero.java
@@ -0,0 +1,9 @@
+package co.edu.uniquindio.Taller_API.entity;
+
+public enum Genero {
+    ACCION,
+    ESTRATEGIA,
+    RPG,
+    DEPORTES,
+    OTROS
+}
