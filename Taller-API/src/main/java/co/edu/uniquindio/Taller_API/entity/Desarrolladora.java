ad783405729f42145c8ed7f7473879d70057a4ec
ValRodriguez97 <valentina.rodriguezc1@uqvirtual.edu.co>
2026-03-02 20:40:42 -0500

    Modificación de application.properties

--- Taller-API/src/main/java/co/edu/uniquindio/Taller_API/entity/Desarrolladora.java
+++ Taller-API/src/main/java/co/edu/uniquindio/Taller_API/entity/Desarrolladora.java
@@ -1,4 +1,13 @@
 package co.edu.uniquindio.Taller_API.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "desarrolladoras")
 public class Desarrolladora {
+
+    @Id
+    @GeneratedValue(strategy)
 }
