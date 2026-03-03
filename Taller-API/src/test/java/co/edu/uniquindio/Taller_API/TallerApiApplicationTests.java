c3ec281c9e00b159862f2a1edb029ac94007ef70
ValRodriguez97 <valentina.rodriguezc1@uqvirtual.edu.co>
2026-03-02 20:31:03 -0500

    feat: initialize Spring Boot project structure
    - Add base package structure (controller, service, repository, entity, exception, config)

---
+++ Taller-API/src/test/java/co/edu/uniquindio/Taller_API/TallerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.Taller_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
