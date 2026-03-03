3ecbc62b5967bbd4af98f9fca21c381170b0b53a
ValRodriguez97 <valentina.rodriguezc1@uqvirtual.edu.co>
2026-03-02 22:46:03 -0500

    Creacion del archivo .json
    POSTMAN

--- Taller-API/src/main/java/co/edu/uniquindio/Taller_API/TallerApiApplication.java
+++ Taller-API/src/main/java/co/edu/uniquindio/Taller_API/TallerApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TallerApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TallerApiApplication.class, args);
 	}
 
